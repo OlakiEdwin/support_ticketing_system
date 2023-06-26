b04e494d23b59dab80ee3e73133eaedf31e09d18
OlakiEdwin <olakiedwin@gmail.com>
2023-06-26 17:56:37 +0300

    Added all files

---
+++ src/main/java/com/example/SupportTicketingApp/SupportTicketingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.SupportTicketingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportTicketingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportTicketingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SupportTicketingApp/SupportTicketingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SupportTicketingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportTicketingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
